0862d016d2f2f04a1df019636a8fed4b5584a3a4
Alf Eaton <alf@hubmed.org>
2014-01-20 09:00:05 +0000

    options object

--- jquery.spotify.js
+++ jquery.spotify.js
@@ -19,10 +19,12 @@
  		lookup: function(uri, params) {
  			var data = $.extend({ uri: uri }, params);
 
- 			return $.spotify.get('lookup/1/', data, true);
+ 			return $.spotify.get('lookup/1/', data, { priority: true });
  		},
 
- 		get: function(path, data, priority) {
+ 		get: function(path, data, options) {
+ 			options = $.extend({ tries: 3 }, options);
+
 			var params = {
 				url: 'http://ws.spotify.com/' + path,
 				data: data,
@@ -30,7 +32,7 @@
 				cache: true,
 			};
 
-			return $.ajaxQueue(params, priority);
+			return $.ajaxQueue(params, options);
 		},
 
 		link: function(url, text) {
